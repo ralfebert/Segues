800dfe6f92428052f248c7072653a1ba155dc8fc
Ralf Ebert <info@ralfebert.de>
2019-09-02 16:44:25 +0200

    Project setup Segues

---
+++ Segues/App/SeguesAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — Segues
+
+import UIKit
+
+@UIApplicationMain
+class SeguesAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
